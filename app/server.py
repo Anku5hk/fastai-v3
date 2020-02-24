350475af7dd1042b74b049eaa1c0e0878fa8d106
Ankush Kuwar <33517094+Anku5hk@users.noreply.github.com>
2020-02-24 16:01:32 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -66,4 +66,4 @@ async def analyze(request):
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
-        app.run(debug=False,port=int(os.getenv('PORT',5000)))
+        uvicorn.run(app=app, port=int(os.getenv('PORT',5000)), log_level="info")
